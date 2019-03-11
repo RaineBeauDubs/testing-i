2f51323f3e45df8fcc4718b587c07f20ab41677c
Raine Wallace <rainebeaudubs@gmail.com>
2019-03-11 17:02:02 -0400

    Added a weapon, I'm not sure if this is how I'm supposed to be doing it or not, but I'm seeing green instead of red in my terminal, and I guess that's all I can ask for. I think my repair is working.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const { dragonboneMace } = require('../items/weapons');
+
+describe('enhancer.js', () => {
+
+  test('can run the tests', () => {
+    expect(true).toBe(true);
+  });
+
+  describe('repair()', () => {
+    it('should return new item w/durability = 100', () => {
+      const item = dragonboneMace;
+      const newItem = enhancer.repair(item);
+      expect(newItem.durability).toBe(100);
+    });
+  });
+});
---
+++ items/weapons.js
@@ -0,0 +1,11 @@
+
+
+const dragonboneMace = {
+  name: 'Dragonbone Mace of the Gods',
+  durability: 97,
+  enhancement: 13,
+}
+
+module.exports = {
+  dragonboneMace,
+}
